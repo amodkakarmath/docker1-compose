7486afb9f7e9decc13eb9b57d382bcd31ac7e462
“Amod” <amodkakarmath@gmail.com>
2026-03-09 22:38:19 +0530

    Updated the app main file index.js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-const mongoose = require ("mongoose")
+const mangoose = require('mongoose');
 
+const DATABASE = 'mongodb://MYMONGO:27017/TESTUP';
+
+mongoose
+    .connect(DATABASE, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('DB Connected');
+    })
+    .catch(() => {
+        console.log('Error in DB Connection');
+    });

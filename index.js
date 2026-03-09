954442fe7bc21f73b36cfacf4b2713310fba1e05
“Amod” <amodkakarmath@gmail.com>
2026-03-09 16:04:35 +0530

    Updated the compose files

---
+++ index.js
@@ -0,0 +1,16 @@
+const mangoose = require('mongoose');
+
+const DATABASE = 'mongodb://MYMONGO:27017/TESTUP';
+
+mongoose
+    .connect(DATABASE, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('DB Connected');
+    })
+    .catch(() => {
+        console.log('DB Connection Error:');
+    });

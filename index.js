ab709f0af3d81ae05c684accc0f5d62b79e25448
Ubuntu <ubuntu@ip-172-31-27-4.ec2.internal>
2026-03-09 17:00:32 +0000

    upadted all

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
-const mangoose = require('mongoose');
+const mongoose = require ("mongoose")
 
-const DATABASE = 'mongodb://MYMONGO:27017/TESTUP';
-
-mongoose
-    .connect(DATABASE, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true
-    })
-    .then(() => {
-        console.log('DB Connected');
-    })
-    .catch(() => {
-        console.log('DB Connection Error:');
-    });
